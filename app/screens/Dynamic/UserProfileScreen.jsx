b24d7fa663b281488b8ba072556eeabb4e677a44
Thea <Thea@DESKTOP-E95RK0P-3.local>
2025-05-08 19:56:32 +0800

    Add sample touchable profile

--- app/screens/Dynamic/UserProfileScreen.jsx
+++ app/screens/Dynamic/UserProfileScreen.jsx
@@ -1,8 +1,9 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { globalStyles } from "@styles/global.style";
 import { TitleDescription } from "@components/CustomUI/";
+import { router } from "expo-router";
 
 const UserProfileScreen = () => {
     return (
@@ -16,13 +17,16 @@ const UserProfileScreen = () => {
                     description="Pick your profile to get started."
                 />
                 <View className="mt-10 flex-1 flex-row flex-wrap gap-4 justify-center">
-                    <View>
+                    <TouchableOpacity
+                        activeOpacity={0.8}
+                        onPress={() => router.replace("/Home")}
+                    >
                         <Image
                             style={styles.profileImage}
                             resizeMode="cover"
                             source={require("@images/ProfileSample.png")}
                         />
-                    </View>
+                    </TouchableOpacity>
                     <View>
                         <Image
                             style={styles.profileImage}
